a62cffe32e7323cd92ea23adfe489729cd6903f7
SkinnyDev <mayolasjan@gmail.com>
2026-09-21 20:45:54 +0200

    Enhance changeFontSize function to select and log spans within the text range

--- CoffeDocs/js/toolbar.js
+++ CoffeDocs/js/toolbar.js
@@ -52,16 +52,39 @@ function changeMarkColor() {
 
 // Canvia la mida de la lletra del text seleccionat i recupera el focus de l'editor.
 function changeFontSize() {
+
 const selection = window.getSelection();
-const spanClear = editor.querySelectorAll("span"); 
 
 if (!selection.rangeCount) {
-  textArea.focus();
-  return;
+    // ...
+    return;
 }
 
-
 const range = selection.getRangeAt(0);
+
+const spans = [];
+
+
+const walker = document.createTreeWalker(
+    range.commonAncestorContainer.parentElement,
+    NodeFilter.SHOW_ELEMENT
+);
+
+let node = walker.nextNode();
+
+while (node) {
+  if (node.tagName === "SPAN") {
+  if (range.intersectsNode(node)) {
+    spans.push(node);
+  }
+
+  
+}
+
+  node = walker.nextNode();
+}
+
+console.log("Spans seleccionats:", spans);
 
 const span = document.createElement("span");
 span.style.fontSize = `${fontSizeSelect.value}px`;
